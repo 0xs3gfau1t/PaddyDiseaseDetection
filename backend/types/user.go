f4ac80ad3d11f73447553bb7a85931ddcb203edc
needl3 <57314527+needl3@users.noreply.github.com>
2023-11-22 08:53:07 +0545

    refactor(types): seperate package for types

---
+++ backend/types/user.go
@@ -0,0 +1,26 @@
+package types
+
+import "github.com/golang-jwt/jwt"
+
+type CreateUserValidInput struct {
+	Name     string `form:"name"`
+	Email    string `form:"email"`
+	Location string `form:"location"`
+	Coord    string `form:"coord"`
+	Password string `form:"password"`
+}
+
+type LoginUserValidInput struct {
+	Email    string `form:"email"`
+	Password string `form:"password"`
+}
+
+type AuthenticatedUserRequestValues struct {
+	Email string `json:"email"`
+	Id    string `json:"id"`
+}
+
+type JwtType struct {
+	jwt.StandardClaims
+	AuthenticatedUserRequestValues
+}
