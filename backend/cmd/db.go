95087916aea14e39d9a3104566044be4aa5f866e
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2024-02-04 18:03:07 +0545

    feat(db): seeding disease and solutions from json

--- backend/cmd/db.go
+++ backend/cmd/db.go
@@ -2,11 +2,16 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
+	"io"
 	"log"
+	"os"
 	"segFault/PaddyDiseaseDetection/ent/migrate"
 	"segFault/PaddyDiseaseDetection/pkg/config"
 	"segFault/PaddyDiseaseDetection/pkg/helpers"
+	"segFault/PaddyDiseaseDetection/types"
 
+	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 )
 
@@ -58,5 +63,65 @@ var migrateReset = &cobra.Command{
 			migrate.WithDropIndex(true),
 			migrate.WithDropColumn(true),
 			migrate.WithForeignKeys(true))
+	},
+}
+
+var seedCmd = &cobra.Command{
+	Use:    "seed",
+	Short:  "Seed disease and solutions to the database.",
+	PreRun: func(cmd *cobra.Command, args []string) { helpers.InjectEnv() },
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client, err := config.NewDbClient()
+		defer client.Close()
+
+		if err != nil {
+			log.Printf("Cannot create db client")
+			return err
+		}
+
+		jsonFile, err := os.Open("seedData.json")
+		if err != nil {
+			log.Printf("Cannot open json file")
+			return err
+		}
+		defer jsonFile.Close()
+
+		byteValue, err := io.ReadAll(jsonFile)
+		if err != nil {
+			log.Printf("Cannot read json file")
+			return err
+		}
+		var seedData types.SeedJson
+		json.Unmarshal(byteValue, &seedData)
+
+		ctx := context.Background()
+
+		for i := 0; i < len(seedData.Disease); i++ {
+			d, err := client.Disease.Create().SetName(seedData.Disease[i].Name).SetID(uuid.New()).Save(ctx)
+			if err != nil {
+				log.Printf("Failed to create disease %s", seedData.Disease[i].Name)
+				log.Println(err)
+				continue
+			}
+			log.Printf("Created disease %s", seedData.Disease[i].Name)
+			for j := 0; j < len(seedData.Disease[i].Solutions); j++ {
+				qb := client.Solution.Create()
+				qb.SetID(uuid.New())
+				qb.SetName(seedData.Disease[i].Solutions[j].Name)
+				qb.SetDescription(seedData.Disease[i].Solutions[j].Description)
+				qb.SetIngredients(seedData.Disease[i].Solutions[j].Ingredients)
+				qb.SetPhotos(seedData.Disease[i].Solutions[j].Photos)
+				qb.AddDisease(d)
+				err = qb.Exec(ctx)
+				if err != nil {
+					log.Printf("Failed to create solution %s", seedData.Disease[i].Solutions[j].Name)
+					log.Println(err)
+					continue
+				}
+				log.Printf("Created solution %s", seedData.Disease[i].Solutions[j].Name)
+			}
+		}
+
+		return nil
 	},
 }
