e7a135de10266cf61ff0d4ae20242514731d2642
needl3 <57314527+needl3@users.noreply.github.com>
2024-02-29 21:40:54 +0545

    feat(ml): restart ml connection to queue for every proc exit with loop

--- terraform/scripts/ml.sh
+++ terraform/scripts/ml.sh
@@ -6,5 +6,4 @@ sudo apt-get install -y python3 python3-pip git
 
 cd /home/$USER/paddydiseasedetection/ai
 sudo pip install -r requirements.txt --no-cache-dir
-while :;do python3 main.py done & # restart this process everytime it's killed due to various reasons
-disown
+nohup bash -c "while :; do python3 main.py; done &" # restart this process everytime it's killed due to various reasons
